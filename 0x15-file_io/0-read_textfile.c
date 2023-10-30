048040859e51540aa4f4448c4b6b361e2f49cc83
Omar Mohamed Abdelrahman Abdelfattah <132802582+omarmohamed7550@users.noreply.github.com>
2023-10-31 01:01:15 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,18 +11,18 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buf;
-	ssize_t fd;
+	ssize_t f;
 	ssize_t w;
 	ssize_t t;
 
 	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	if (f == -1)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
+	t = read(f, buf, letters);
 	w = write(STDOUT_FILENO, buf, t);
 
 	free(buf);
-	close(fd);
+	close(f);
 	return (w);
 }
