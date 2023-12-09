314f1874d642188319e273fde9dac8e94e8178c2
Omar Mohamed Abdelrahman Abdelfattah <132802582+omarmohamed7550@users.noreply.github.com>
2023-12-09 04:08:49 +0300

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,1 +1,31 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of
+ * dlistint_t list
+ *
+ * @x: head of the list
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *x)
+{
+	int count;
+
+	count = 0;
+
+	if (x == NULL)
+		return (count);
+
+	while (x->prev != NULL)
+		x = x->prev;
+
+	while (x != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		x = x->next;
+	}
+
+	return (count);
+}
 
