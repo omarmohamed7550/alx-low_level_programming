9415237a627fa73429d24a55f66575c35faf15b8
Omar Abdelfattah <omaromar1234641@gmail.com>
2023-12-21 21:16:56 -0800

    task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ *
+ * Return: pointer to the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table;
+	unsigned long int index;
+
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+		return (NULL);
+	new_table->size = size;
+	new_table->array = malloc(size * sizeof(hash_node_t *));
+	if (new_table->array == NULL)
+	{
+		free(new_table);
+		return (NULL);
+	}
+	for (index = 0; index < size; index++)
+		new_table->array[index] = NULL;
+	return (new_table);
+}
